b91783d7a875294f5f4abd00aab14c7a1f427ef8
davidbogomolov <david.bogomolov2@gmail.com>
2023-04-30 13:41:23 +0300

    last version of project

---
+++ src/components/pages/404.js
@@ -0,0 +1,22 @@
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import {Link} from 'react-router-dom';
+import {Helmet} from "react-helmet";
+
+const Page404 = () => {
+    return (
+        <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="error page"
+                />
+                <title>Page 404</title>
+            </Helmet>
+            <ErrorMessage/>
+            <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
+            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>
+        </div>
+    )
+}
+
+export default Page404;
---
+++ src/utils/setContent.js
@@ -0,0 +1,25 @@
+import Skeleton from "../components/skeleton/Skeleton";
+import Spinner from "../components/spinner/Spinner";
+import ErrorMessage from "../components/errorMessage/ErrorMessage";
+
+
+const setContent=(process,Component,data)=>{
+    switch(process){
+        case 'waiting':
+            return  <Skeleton/>;
+            break;
+        case 'loading':
+            return <Spinner/>;
+            break;
+        case 'confirmed':
+            return   <Component data={data}/>;
+            break;
+        case 'error':
+            return  <ErrorMessage/>;
+            break;
+        default:
+            throw new Error('Unexpected process state')
+    }
+}
+
+export default setContent;
